4b1f162eaf475258fc7338e78fe7966527c99f4d
MargheritaMottana <margherita.mottana@gmail.com>
2024-07-16 17:33:34 +0200

    Scrittura html di "dadi"

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -1,8 +1,33 @@
 /* 
+
 Gioco dei dadi
+
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 Prima di partire a scrivere codice poniamoci qualche domanda:
 Che ci sia un array da qualche parte?
 Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+step
+
+1. creare un bottone per il pc
+2. all'input del bottone 
+-- generare un numero random per il pc
+-- scrivere in pagina il numero del pc
+
+3. creare un bottone per il giocatore
+4. all'input del bottone
+-- generare un numero random per il giocatore
+-- scrivere in pagina il numero del giocatore
+
+4. comparare i numeri
+5. se giocare > pc
+-- allora il giocatore ha vinto
+
+6. se giocatore < pc
+-- allora il giocatore ha perso
+
+7. scrivere in pagina chi ha vinto
+
 */
+
