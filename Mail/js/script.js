bddb7d9a155a990d584dfb030edfaa0a316c2021
MargheritaMottana <margherita.mottana@gmail.com>
2024-07-16 15:10:15 +0200

    creata cartella con file html e js per ogni esercizio

---
+++ Mail/js/script.js
@@ -0,0 +1,15 @@
+/*
+Mail
+Chiedi all'utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull'esito del controllo. 
+
+step
+1. creare una lista di utenti che possono accedere 
+2. chiedere all'utente la sua mail con un prompt o con un form
+3. verificare che le credenziali dell'utente siano tra quelle della lista
+    se sì -->
+      allora mostrare messaggio di avvenuto accesso
+    se no -->
+       negare l'accesso
+*/
